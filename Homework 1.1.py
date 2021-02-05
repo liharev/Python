62220c00734502a94a316a7a5deb8e07fd01175e
liharev <liharev@gmail.com>
2021-02-05 14:27:11 +0300

    Homework for lesson 2

---
+++ Homework 1.1.py
@@ -0,0 +1,6 @@
+# Homework 1.1
+name = str (input("Введите Ваше имя: "))
+year = int (input("Введите год Вашего рождения: "))
+age = 2021 - year
+print(f"В этом году,{name}, Вам стукнет {age} лет. Пора учить Python!")
+print("Конец программы")
