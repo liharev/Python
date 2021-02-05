8734652d7fbdc209d72c656d32a09001c7fde159
liharev <liharev@gmail.com>
2021-02-05 14:30:17 +0300

    Removed all files excluding homework for lesson 2

--- Homework 1.3.py
+++
@@ -1,4 +0,0 @@
-# Homework 1.3
-digit = int (input("Введите цифру (n) от 0 до 9: "))
-print(f"Сумма n + nn + nnn: {digit+int(str(digit)+str(digit))+int(str(digit)+str(digit)+str(digit))}")
-print("Конец программы")
